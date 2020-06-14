a9d1e3551351f4e3ff6b4901001b7addb37439c8
Mit-Patel <patelmit2012@gmail.com>
2020-06-15 00:28:22 +0530

    Day 1 - Life Cycle and States of a Component

---
+++ src/Application.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Application extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+    
+    handleClick = () => {
+        this.setState({count: this.state.count + 1});
+    }
+   
+
+    componentDidUpdate(props, state){
+        if(this.state.count > 10 && this.state.count !== state.count && !this.state.overTen){
+            this.setState({overTen:true});
+        }
+    }
+    render() {
+        let {count} = this.state;
+
+        return (
+            <div>
+                <h1> You clicked the button {count} times!</h1>
+                {
+                    (this.state.overTen)?
+                    <h3>Beat the highscore to 10!!</h3>
+                    :null
+                }                
+                <span>
+                    <button onClick={(e) => this.handleClick()}>Click Me</button>
+                </span>
+            </div>
+        );
+        }
+    }
+
+
+    export default Application
